95777d9cf2d4e6b2b6c67f52cf7fdccc61ab71fe
kennethn23 <kennethng1337@gmail.com>
2024-04-03 08:49:42 -0400

    blank

--- vite-project/src/router/index.ts
+++ vite-project/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// @ts-ignore
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -15,6 +16,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      // @ts-ignore
       component: () => import('../views/AboutView.vue')
     }
   ]
