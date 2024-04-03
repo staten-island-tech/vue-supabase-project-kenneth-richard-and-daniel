022a06da4dc022ddef3c40e0a6bf0ed873a4f74c
kennethn23 <kennethng1337@gmail.com>
2024-04-03 09:23:57 -0400

    a

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    
+    <li v-for=""></li>
   </div>
 </template>
 
 <script setup lang="ts">
+
+import { ref, onMounted } from 'vue';
+import { supabase } from '@/lib/supabaseClient';
+
+const users: any = ref([]);
+const loaded: object = ref(false);
+
+async function getUsers () {
+  const { data } = await supabase.from('users').select();
+  users.value = data;
+}
+
+onMounted(() => {
+  getUsers();
+});
 
 </script>
 
