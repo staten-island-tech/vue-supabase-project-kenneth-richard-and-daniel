2475f62246d462ba7a0e4ea4ae795c75b116f1dc
kennethn23 <kennethng1337@gmail.com>
2024-04-17 08:57:49 -0400

    a

--- vite-project/src/components/LoginAuth.vue
+++ vite-project/src/components/LoginAuth.vue
@@ -8,14 +8,13 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-// @ts-ignore
 import { supabase } from '@/lib/supabaseClient';
 
 const loading = ref<boolean> (false);
 const password = ref<string> ("");
 const email = ref<string> ("");
 
-/*const handleLogin = async () => {
+const handleLogin = async () => {
   try {
     loading.value = true;
     const { data, error } = await supabase.auth.signInWithPassword({
@@ -31,24 +30,6 @@ const email = ref<string> ("");
     }
   } finally {
     loading.value = false;
-  }
-}*/
-
-async function handleLogin() {
-  try {
-    // Sign up the user
-    const userData = await supabase.auth.signUp({
-      email: email.value,
-      password: password.value
-    });
-
-    await supabase
-      .from('users')
-      .insert({ uuid: userData.data.user.id, email: email.value })
-    console.log(userData);
-
-  } catch (error: any) {
-    console.error('Sign-up error:', error.message);
   }
 }
 </script>
